237b2219ec900730fa99935cfcd1d25c3c5dc629
niku <niku@niku.name>
2018-02-25 08:22:48 +0900

    Parse yaml from given path

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
 import fs from "fs";
+import yaml from "js-yaml";
+import process from "process";
 
-// tslint:disable-next-line:no-console
-fs.readFile("package.json", (err, data) => console.log(data.toString()));
+const [execPath, javascriptFilePath, inputPath, ...rest] = process.argv;
+
+fs.readFile(inputPath, "utf8", (err, data) => {
+  if (err) {
+    // tslint:disable-next-line:no-console
+    console.log(err);
+    return;
+  }
+
+  try {
+    const doc = yaml.safeLoad(data);
+    // tslint:disable-next-line:no-console
+    console.log(doc);
+  } catch (e) {
+    // tslint:disable-next-line:no-console
+    console.log(e);
+  }
+});

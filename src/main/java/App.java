edf334794917b86ca4ee391a74155c27a27f2a6b
niku <niku@niku.name>
2018-04-10 18:47:08 +0900

    Use PMD
    https://docs.gradle.org/current/userguide/pmd_plugin.html
    
    And, add basic PMD rule sets
    
    > Instead, start with some selected specific rules, e.g. the rules that dete
    ct unused code from the category Best Practices and fix any unused locals and fi
    elds.
    > Then, run rules, that detect empty if statements and such-like. You can fi
    nd these rules in the category Error Prone.
    
    https://pmd.github.io/pmd-6.2.0/pmd_userdocs_best_practices.html

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 public class App {
+
+  @SuppressWarnings("PMD.SystemPrintln")
   public static void main(String... args) {
     final boolean result = new Library().someLibraryMethod();
     System.out.println(result);

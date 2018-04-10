0c3cd792e8942e152aa1ed0934ae4055b97c75ca
niku <niku@niku.name>
2018-04-10 18:17:43 +0900

    Apply checkstyle setting

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
-    public static void main(String... args) {
-        final boolean result = new Library().someLibraryMethod();
-        System.out.println(result);
-    }
+  public static void main(String... args) {
+    final boolean result = new Library().someLibraryMethod();
+    System.out.println(result);
+  }
 }
